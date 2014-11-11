fe161d830756f664210734d7b9b7c3613834a7c2
Chris--A <chris@genx.biz>
2014-11-12 02:13:34 +1000

    Added more and updated current mods

--- Moduino.js
+++ Moduino.js
@@ -24,14 +24,63 @@ var Moduino = {
 			
 			'removeShopping' : true
 		},
+		
+		'index' : {
+			'enabled' : true,
+			
+			'root' : {
+				'enabled' : true
+			},
+			
+			'forum' : {
+				'enabled'			: true,
+				'shrinkContainers'	: true,
+				'removeMenu'		: true,		/** There is currently an empty menu bar above thread listing. (17px high) **/
+				
+				'sticky' 			: {
+					'enabled'	: true,			
+					'hide'		: true,			/** Hide sticky threads **/
+					'addToggle'	: true			/** Add a button to toggle visibility **/
+				}	
+			}
+		
+		},
 	
 		'thread' : {
-			'enabled' 		: true,		
-			'sizeableCode'	: true,
-			'hideIP'		: true,
-			'shrinkPost'	: true,
-			'combineKarma'	: true,				/** Combine the '[Add Karma]' link with the karma count **/
-			'hideProfileLink' : true			/** Currently a users post profile shows two web links **/
+			'enabled' 			: true,	
+
+			'code'				: {
+				'enabled' 		: true,
+				'hoverSelect'	: true
+			},
+			
+			'quote'				: {
+				'enabled' 		: true,
+				'hoverSelect'	: true,
+				'hideEmptyHead'	: true,
+				'colours'		: [ '#e4e4e4', 'rgba(250, 242, 0, 0.28)', 'rgb(245, 215, 120)' ]
+			},
+			
+			'sizeableCode'		: {
+				'enabled'		: true,			/** Can be set to false if using highlighting. (value is used). **/
+				'value'			: 250			/** Maximum starting height for long code boxes **/
+			},
+			
+			'hideIP'			: true,			/** No need to know everyone's IP has been logged. **/
+			'shrinkPost'		: true,			/** Minor padding & margin fixes. **/
+			'combineKarma'		: true,			/** Combine the '[Add Karma]' link with the karma count **/
+			'hideProfileLink'	: true,			/** Currently a users post profile can show two web links **/
+			
+			'codeHighlighting'	: {
+				'enabled'		: true,
+				'lineNumbers'	: true,
+				'pathToJS'		: '//arduino.land/JS/SyntaxHighlighter/',
+				'pathToCSS'		: '//arduino.land/CSS/SyntaxHighlighter/',
+				'core'			: 'Moduino',
+				'brushes'		: [ 'Cpp' ],
+				'theme'			: 'Moduino',
+				'cssExt'		: 'scss'
+			}
 		},
 		
 		/***
@@ -76,7 +125,71 @@ var Moduino = {
 		DBG();
 	},
 	
-	'dbgm' : function ( msg ){ DBG( msg, Moduino.debug.mod ); }
+	'dbg'	: function ( msg, code ){ DBG( msg, code || Moduino.debug.notification ); },
+	'dbgm'	: function ( msg ){ this.dbg( msg, Moduino.debug.mod ); }
+	
+};
+
+/***
+	Helper functions and jQuery add ons.
+***/
+
+function formatButton( b ){
+	return $( b ).css({
+		'padding'			: 0,
+		'margin'			: 0,
+		'min-height'		: 'inherit',
+		'font-size'			: '0.8em',
+		'line-height'		: '1.1em',
+		'letter-spacing'	: '0',
+		'width'				: 'auto'
+	});
+}
+
+function selectElementText(el, win) {
+    win = win || window;
+    var doc = win.document, sel, range;
+    if (win.getSelection && doc.createRange) {
+        sel = win.getSelection();
+        range = doc.createRange();
+        range.selectNodeContents(el);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (doc.body.createTextRange) {
+        range = doc.body.createTextRange();
+        range.moveToElementText(el);
+        range.select();
+    }
+}
+
+jQuery.cachedScript = function( url, options ) {
+  options = $.extend( options || {}, {
+    dataType: "script",
+    cache: true,
+    url: url
+  });
+  return jQuery.ajax( options );
+};
+
+$.fn.innerText = function(msg) {
+	 if (msg) {
+		if (document.body.innerText) {
+		   for (var i in this) {
+			  this[i].innerText = msg;
+		   }
+		} else {
+		   for (var i in this) {
+			  this[i].innerHTML.replace(/\&lt;br\&gt;/gi,"\n").replace(/(&lt;([^&gt;]+)&gt;)/gi, "");
+		   }
+		}
+		return this;
+	 } else {
+		if (document.body.innerText) {
+		   return this[0].innerText;
+		} else {
+		   return this[0].innerHTML.replace(/\&lt;br\&gt;/gi,"\n").replace(/(&lt;([^&gt;]+)&gt;)/gi, "");
+		}
+	 }
 };
 
 /*** 
@@ -101,13 +214,16 @@ $( function ( gq ){
 	switch( $( 'title' ).first().text() ){
 	
 		case 'Arduino Forum - Index':	isGlobalIndex	= true; break;
-		case 'Post Reply': 				isNewReply		= true; break;			
+		case 'Post Reply': 				isNewReply		= true; break;
+		default:
+		
+			//Only threads should contain a #tob_subject element.
+			isThread = !!$( 'span#top_subject' ).length;
+			
+			//Detect forum index.
+			isForumIndex = ( /http:\/\/forum.arduino.cc\/index.php\?board=[0-9.]*/ ).test( dhr );
 	}
-	
 
-	//Only threads should contain a #tob_subject element.
-	isThread = !!$( 'span#top_subject' ).length;
-	
 	Moduino.init();
  
 	switch( bv( isGlobalIndex ) +
@@ -147,7 +263,10 @@ function DBG( str, level ){
 	
 	var thisConfig = Moduino[ 'config' ][ 'internal' ];
 
-	str = str || "Thank you for using Moduino\r\nWritten by:	Christopher Andrews";
+	str = str ||	"/r/n=======================================\r\n" +
+					"Thank you for using Moduino\r\n" +
+					"Written by:	Christopher Andrews" +
+					"/r/n=======================================\r\n";
 	level = level || Moduino.debug.notification;
 	
 	
@@ -219,13 +338,27 @@ function GlobalMods(){
 	
 		Moduino.dbgm( 'minimizeHeader' );
 		
+		//Remove colour from page.
 		$( 'body' ).css( 'background', 'none' );
 		$( '#pageheader' ).css({
 			'margin-top'	: '5px',
-			'padding-top'	: '3px',
-			'padding-left'	: '3px'
-		});
-		$( 'nav.top-bar' ).css( 'margin-right', '3px' );
+			'padding-top'	: '3px'
+		})
+			.children( 'div.row' )
+			.first()
+			.css( 'padding-left', '3px' );
+		
+		$( 'nav.top-bar' ).css( 'margin', '0 3px 0 0' );
+		
+		//Remove margin from bread crumbs and move to header navigation.
+		$( '#headerbread' )
+			.css({
+				'margin' : '0px',
+				'padding' : '0 3px 0 3px'
+			}).appendTo( '#pageheader' );
+		
+		//Move bread crumbs into header navigation.
+		
 		
 		/** If the content width has been modified, this  option will use the same value for the header. **/
 		
@@ -236,6 +369,8 @@ function GlobalMods(){
 				'width'		: config.resizeContent.value + '%'
 			});
 		}
+		
+		
 	}
 	
 	/** Hide the damn shopping cart. **/
@@ -258,8 +393,45 @@ function GlobalIndexMods(){
 	ForumIndexMods function.
 ***/
 
-function ForumIndexMods(){ 
+function ForumIndexMods( mode ){ 
 	DBG( 'Found forum index' );
+	
+	var config = Moduino.config.index.forum;
+	
+	if( config.shrinkContainers ){
+	
+		Moduino.dbgm( 'shrinkContainers' );
+		$( '#topic_container>.windowbg' )
+			.height( 'auto' )
+			.css({ 'border' : '0' })
+			.find( 'p' )
+			.css( 'min-height' , 'inherit' );
+	}
+			
+	/** There is currently an empty menu bar above thread listing. (17px high) **/
+	
+	if( config.removeMenu ){
+	
+		Moduino.dbgm( 'removeMenu' );
+		$( '#wrapper>#upper_section' ).hide();
+	}
+	
+	/** Hide sticky threads **/
+	var sel = '.sticky-forum.windowbg';
+	
+	if( config.sticky.enabled && $(sel).length ){
+	
+		Moduino.dbgm( 'sticky' );
+		
+		if( config.sticky.hide ) $(sel).hide();
+			
+		if( config.sticky.addToggle ){
+			$( '#description_board' ).prepend( '<button id="sticky-toggle" onClick="' + "$( '" + sel + "' ).toggle();" + '">Sticky Threads</button>' );
+			formatButton( '#sticky-toggle' ).css( 'float','right' );
+		}
+	}	
+	
+	
 }
 
 /***
@@ -275,15 +447,16 @@ function ThreadMods(){
 	
 	/*** Allow resizing of code containers ***/
 	
-	if( config.sizeableCode ){
+	if( config.sizeableCode.enabled ){
 	
 		Moduino.dbgm( 'sizeableCode' );
 		
-		$( function(){
-			$( '.bbc_code' ).each( function( i, e ){ 
-				$(e).css({ 'max-height' : 'none', 'height' : ( e.scrollHeight > 200 ? '200px' : 'auto' ) });
+		$( '.bbc_code' ).each( function( i, e ){ 
+			$(e).css({ 
+				'max-height' : 'none', 
+				'height' : ( e.scrollHeight > config.sizeableCode.value ? config.sizeableCode.value + 'px' : 'auto' ) 
 			});
-		});	
+		});
 	}
 	
 	/*** Hide IP information ***/
@@ -315,12 +488,8 @@ function ThreadMods(){
 	if( config.combineKarma ){
 	
 		Moduino.dbgm( 'combineKarma' );
-		
-		$( 'li.custom.karma_good' ).each( function( i, e ){
-			e = $(e);
-			i = e.first().next().next().find( 'a' ).first();
-			e.html( '<a href="' + i.attr( 'href' ) + '">' + e.text() + '</a>' );
-			i.hide();
+		$( 'li.custom.karma_good' ).wrapInner(function(){ 
+			return '<a href="' + $(this).siblings( '.karma_labels' ).hide().children( 'a' ).attr( 'href' ) + '"></a>'; 
 		});
 	}
 	
@@ -332,7 +501,150 @@ function ThreadMods(){
 		$( 'li.link-profile' ).hide();
 
 	}
+	
+	/** Add highlighting to code boxes **/
+	
+	if( config.codeHighlighting.enabled && $( '.bbc_code' ).length ){
+	
+		var chl = config.codeHighlighting,
+		txt;
+	
+		Moduino.dbgm( txt = 'codeHighlighting' );
+		txt += ': ';
+		
+		//Insert syntax highlighter CSS
+		$('head')
+			.append('<link rel="stylesheet" href="' + chl.pathToCSS + 'shCore.' + chl.cssExt + '" type="text/css" />')
+			.append('<link rel="stylesheet" href="' + chl.pathToCSS + 'shCore' + chl.core + '.' + chl.cssExt + '" type="text/css" />')
+			.append('<link rel="stylesheet" href="' + chl.pathToCSS + 'shTheme' + chl.theme + '.' + chl.cssExt + '" type="text/css" />');		
+
+		//Get syntaxhighlighter code.
+		$.cachedScript( chl.pathToJS + 'shCore.js' )
+			.fail(function ( s ){ Moduino.dbg( txt + 'AJAX Failed', Moduino.debug.error ); })
+			.success( function ( s ){
+			
+				Moduino.dbg( txt + 'Got shCore.js' );
+				
+				var successCount = 0;
+				
+				$.each( chl.brushes, function ( i, e ){
+				
+					$.cachedScript( chl.pathToJS + 'shBrush' + e + '.js' )
+						.fail(function ( s ){ Moduino.dbg( txt + 'AJAX Failed', Moduino.debug.error ); })
+						.success( function ( s ){
+						
+							Moduino.dbg( txt + 'Got shBrush' + e + '.js' ); 
+							
+							if( ++successCount == chl.brushes.length ){
+							
+								$( 'code.bbc_code' )
+									.css({
+										'padding'		: '0px',
+										'max-height'	: 'none',
+										'border'		: '1px solid #cfcfcf',
+										'position'		: 'relative'
+									})
+									.wrapInner( '<pre class="brush: cpp"></pre>' )
+									.children()
+									.each( function ( i, e ){ $(e).innerText( $(e).innerText() ); });
+									
+								SyntaxHighlighter.defaults.gutter = chl.lineNumbers;
+								SyntaxHighlighter.defaults.toolbar = false;
+								SyntaxHighlighter.highlight();
+								
+								if( config.code.hoverSelect  )
+									addHoverSelect( '.bbc_code', "$(this).parent().find( 'div.container' )[0]" );
+
+								//Fix code box height, as highlighted code is smaller than the standard SMF code.
+								$( '.bbc_code' ).each( function( i, e ){ 
+									$(e).css( 'height', ( $(e).find( '.syntaxhighlighter' )[0].scrollHeight > config.sizeableCode.value ? config.sizeableCode.value + 'px' : 'auto' ) );
+								});
+							}
+						});
+				});				
+		});
+	}
+	
+	/*** Move code '[Select]' function to hover over ***/
+	
+	if( config.code.hoverSelect  ){
+	
+		Moduino.dbgm( 'code.hoverSelect' );
+		$( '.codeheader' ).hide();
+		if( !config.codeHighlighting.enabled )
+			addHoverSelect( '.bbc_code' );
+	}
+	
+	if( config.quote.enabled ){
+
+		Moduino.dbgm( 'quote' );
+	
+		$( '.quoteheader' ).each(function ( i, e ){
+			if( config.quote.hideEmptyHead && $(e).children().text() === 'Quote' )
+				$(e).hide();
+			else
+				$(e).css( 'padding', '0 4px' );
+		});
+		
+		
+		
+		if( config.quote.hoverSelect  ){
+		
+			Moduino.dbgm( 'quote.hoverSelect' );
+			$( '.bbc_standard_quote' ).wrapInner( '<div></div>' );
+			addHoverSelect( '.bbc_standard_quote', "$(this).parent().find('div').first()[0]" )
+				.css( 'padding', '4px 4px' );
+		}
+	
+		if( config.quote.colours != false ){
+		
+			Moduino.dbgm( 'quote.colours' );
+			
+			$( '.bbc_standard_quote' ).css({
+				'background-color' : config.quote.colours[ 1 ],
+				'border' : '1px solid ' + config.quote.colours[ 2 ],
+				'margin-bottom' : '-1px'
+			});
+			
+			$( '.quoteheader' ).css({
+				'background-color' : config.quote.colours[ 0 ],
+				'border' : '1px solid ' + config.quote.colours[ 2 ],
+				'margin-bottom' : '-1px'
+			});			
+		}
+	}
+	
 	return;
+}
+
+function addHoverSelect( owner, what ){
+
+		what = what || 'this';
+
+		var handler = function(){
+			$( '.select-all-hover' )
+				.filter( ':visible' )
+				.each(function( i, e ){
+					$(e).css( 'top', $(e).parent().offset().top - $(window).scrollTop() );
+			});
+		};
+		
+		$( owner )
+			.append( '<button class="select-all-hover" onClick="' + "selectElementText(" + what + ");" + '">Select All</button>' )
+			.on('mouseenter', function () {
+				$(this)
+					.children( '.select-all-hover' )
+					.show();
+				handler();
+				$(window).bind( 'scroll', handler );
+			}).on('mouseleave', function () {
+				$(this).children( '.select-all-hover' ).hide();
+				$(window).unbind( 'scroll', handler );
+		});
+		formatButton( '.select-all-hover' )
+			.css( 'position', 'fixed' )
+			.hide();
+		return $( owner );
 }
 
 /***
@@ -344,6 +656,18 @@ function ReplyMods(){
 }
 
 
+/*
+'<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">'
+ + '<input type="hidden" name="cmd" value="_donations">'
+ + '<input type="hidden" name="business" value="GYFSELTGAYHEY">'
+ + '<input type="hidden" name="lc" value="AU">'
+ + '<input type="hidden" name="item_name" value="Moduino">'
+ + '<input type="hidden" name="currency_code" value="AUD">'
+ + '<input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_LG.gif:NonHosted">'
+ + '<input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">'
+ + '<img alt="" border="0" src="https://www.paypalobjects.com/en_AU/i/scr/pixel.gif" width="1" height="1">'
+ + '</form>'
+*/
 /*** UNUSED CODE
 
 function isDef( d ){
@@ -356,6 +680,15 @@ function isObj( obj ){
 
 function isFunc( func ){
 	return !isDef( func ) && typeof( func ) == "function";
+}
+
+function htmlEscape(str) {
+    return String(str)
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
 }
 ***/
 
--- README.md
+++ README.md
@@ -6,17 +6,36 @@ Moduino is a JavaScript modding system for the public forum found at http://foru
 Written by:	Christopher Andrews
 Licence:	Released using MIT licence.
 
-There are currently 8 modifications available. Each mod can be disabled in the config.
+There are currently 17 modifications available. Each mod can be disabled in the config.
 
 ### Global forum modifications
+
 * Resize width of content.
 * Shrink the page banner.
 * Remove shopping icon.
 
+### Forum index modifications
+
+* Shrink content
+* Hide sticky threads
+* Add toggle for sticky threads.
+
 ### In thread modifications
-* Allow code boxes to be resizeable.
-* Shrink post containers.
-* Combine *[Add Karma]* link with user karma count.
-* Hide IP text
-* Hide secondary website link.
+
+* Post Mods
+  * Shrink post containers.
+  * Profile mods
+    * Combine *[Add Karma]* link with user karma count.
+    * Hide IP text
+    * Hide secondary website link.
+  * Code box mods
+    * Allow code boxes to be resizeable.
+    * Syntax highlighting
+    * Line numbers
+    * Move select all to a hover over.
+  *Quote mods
+    * Hide empty headers
+	* Change colours
+	* Add select all hover over.
+
 
